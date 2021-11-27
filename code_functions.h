e1dbb49fdc6636619d4dac87d3327e00ef0f36c1
Hector <hectorsucre13@gmail.com>
2021-11-27 09:00:31 -0600

    adding necessary files

---
+++ code_functions.h
@@ -0,0 +1,9 @@
+void help(){
+    cout << "HELP FUNCTION" << endl;
+    cout << "-h or --help       :   to acces ussage parameters or comments." << endl;
+    cout << "-f or --file       :   for setting the route and name of the file." << endl;
+    cout << "                       example:    main.exe -d route/documentName.txt" << endl;
+    cout << 
+
+}
+
